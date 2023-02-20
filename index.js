3565dda7322727d298315addcc166c2ed6285f6d
Al Shaharia <ifty682002@gmail.com>
2023-02-20 18:18:55 +0600

    member repots show

--- index.js
+++ index.js
@@ -259,6 +259,14 @@ async function run() {
       res.send(result);
     });
 
+    // get member reports
+    app.get("/user/reports", verifyJWT, verifyMember, async (req, res) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const result = await reportCollection.find(query).toArray();
+      res.send(result);
+    });
+
     // get all notification
     app.get("/get-notification", verifyJWT, verifyMember, async (req, res) => {
       const query = {};
